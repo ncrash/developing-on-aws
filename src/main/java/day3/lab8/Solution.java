9c21a9fd05a9eac3f7f6c55065ab3e8208754250
Daekwon Kang <daekwon.kang@gmail.com>
2017-09-22 14:55:06 +0900

    Day 3 - lab8 소스코드 커밋

---
+++ src/main/java/day3/lab8/Solution.java
@@ -0,0 +1,28 @@
+package day3.lab8;// Copyright 2017 Amazon Web Services, Inc. or its affiliates. All rights reserved.
+
+import net.spy.memcached.MemcachedClient;
+
+import java.net.InetSocketAddress;
+
+public class Solution {
+
+  public static MemcachedClient newMemcachedClient(String host, int port) {
+    MemcachedClient memcachedClient = null;
+    try {
+      memcachedClient = new MemcachedClient(new InetSocketAddress(host, port));
+    } catch (Exception e) {
+      // Do nothing
+    }
+    return memcachedClient;
+  }
+
+  public static Object getCacheItem(MemcachedClient memcachedClient, String key) {
+    Object valueFromCache = null;
+    valueFromCache = memcachedClient.get(key);
+    return valueFromCache;
+  }
+
+  public static void setCacheItem(MemcachedClient memcachedClient, String key, Object value) {
+    memcachedClient.set(key, 3600, value);
+  }
+}
